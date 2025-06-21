7ea05d83e45d25b411d7deec86d3a372d377de9f
الأيقونة للعطور <147558813+talalye1@users.noreply.github.com>
2025-06-21 03:09:26 +0300

    Add files via upload
    Signed-off-by: الأيقونة للعطور <147558813+talalye1@users.noreply.github.com>

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'icon-perfumes-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/moan4.jpg',
+  '/men.png',
+  '/men1.png',
+  '/men2.png',
+  '/men3.png',
+  '/men4.png',
+  '/men5.png',
+  '/female.png',
+  '/female1.png',
+  '/female2.png',
+  '/female3.png',
+  '/female4.png',
+  '/female5.png',
+  '/moan.png',
+  '/moan1.png',
+  '/moan2.png',
+  '/moan3.png',
+  '/moan4.png',
+  '/moan5.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  '/screenshot1.png',
+  '/screenshot2.png',
+  '/screenshot3.png',
+  '/screenshot4.png',
+  '/screenshot5.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
